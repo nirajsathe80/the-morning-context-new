2256c515f61f53a9794e60b721a7a5c2c417499d
nirajsathe80 <nirajsathe80@gmail.com>
2024-11-13 11:14:26 +0530

    common data file add

---
+++ app/data/index.ts
@@ -0,0 +1,129 @@
+export const featuredStoriesData = [
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/c01d/9f9a/6f38c048c3ba6fdda30589521ca80cd8?Expires=1732492800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EIv-iAXd0meJZKa5Jm229dzTX2GebXwk4hP3rP~yBArSWOP2CMqeIhv-Rtm0tlvAtW4o8R8s-1gKAEvWPv5ufUaDxPmUZe~lqYu31NauSTidymHYj2dwz6PVkGdcwYJ46rSRgFRL7~s6xge7OI96QZZT017p-MD8lv9-cT1WLJxaQu8eyM2-MoO1edYHVywFMuBKGzqEUU8Z7RmK32DP7QnNYw9MvPp7ocMMUABJLJ5rG6jM6MDYeMZcrF~a5f8lYK1r9CQPDXuPDMilZImRZwTjJpdDALh0SMWyhO9My9K2qb7FyHKMK0M56qs4zpljc3EsszDJkHYRlN0YRxeDYg__",
+    title: "Is Bandhan banking too much on microfinance?",
+    description: `  In trying to keep costs in check at his loss-making airline, chairman Ajay Singh may have created a toxic work atmosphere and poor safety culture.`,
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    articleReadTime: 5,
+    commentsCount: 23,
+  },
+];
+
+export const LatestStoriesData = [
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/460b/7ba2/cef7f3b55ea636ab34295402fb4c9c30?Expires=1732492800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XeOd0EixisX5Mf-vkl8xYREMNWMSEuiJuW7KWMJWxvX8X3HBnZcE2QKOgIXxNtSgX5hsMczjhXgn59yEncGO-wki5TWCguZxSCKOrcLqvYhoReJzLAbqJZPhOcn~liV8TsnZQI5gt7BcpVEbzIa0BGSTZkkfZObZ7S8FFu-dk2w-sN0tC04hIUdqQW19jzXXS10HjgHPb4sumyRLPIH1qdlSHn~pAFClhFeCA~LNbCUd2XwQ94aRICMjp-Mfj1Ht6RfiRAnUzbAV4Ai1aviS8AV3YxrQ0uCXi7Ymo5~MNaiTkaVR2cFg0WaNMmMmr~Uv~6pQAw-NHUtWZKZga~p5OQ__",
+    title: "Is Bandhan banking too much on microfinance?",
+    description: `  In trying to keep costs in check at his loss-making airline, chairman Ajay Singh may have created a toxic work atmosphere and poor safety culture.`,
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    articleReadTime: 5,
+    commentsCount: 23,
+  },
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/2f8d/35e9/ae7eee1b1d07e829b629c397cb4d2849?Expires=1732492800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UAGq1Np~VPQwHXdTwX1mupn99Vc15uSc11gQEk-FQkDWTg8iM8O6WJWa3q3fy2S5Dq-LMxV-WbULMbYtjTZNuddsnVvYarq9vsnseagFjYF7N9PYKuctNBKXmg886uZF1C8FSC0~ZjGfJUgZne9stiX83Wa48h-gYv9bp4jvBTEZFULBd9PJxJ9fmUwmaQy~UFumX7sMySX7H-CGKk57pjY1EIN7~VmnKckaLcVQgxTre5IOth3HcEI~~hsXjgGg46wpVCrzWx2mEskz-js8bwd8v-HneWAQ4V9lg0Eq6F3hyCOB2gMJRnAaJghnAS7FvKeoiqE4cZkF6t~osCVdrg__",
+    title: "Is Bandhan banking too much on microfinance?",
+    description: `  In trying to keep costs in check at his loss-making airline, chairman Ajay Singh may have created a toxic work atmosphere and poor safety culture.`,
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    articleReadTime: 5,
+    commentsCount: 23,
+  },
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/dc14/c168/a437cbcac0ff201ec24f2c85a9f04d55?Expires=1732492800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YbQGxN0I-aQOP2Tj92m~9GJR-QlcjUNRYY8vhBPWmG~rRLbf5cLy6WwuH6pfFKsCtoTCGRxZWln5uI8ky5A-qcChOPyn0GdGUaW7Vb74NPi-sjky4zG7lmbwheyAHkZMRIQsorberL8p~zgzb8BVNDsDT6Y6FIq1I2BB7L~-wcyQQZOCT6oyI3oUkqL3Z6oaG5zXMjMQ0zoKSRk0NAWfKsmVvrdr5w6p-v53Sm7uW6DmcU3HtQrLfONI9ABrGiHsCBlgKNC-feiD43MUXV6ET8I7WRD848Q8o~HsNmjnZA~Gj2GXTZADq97RrE7OEx0btVDpg1gJxan5XJMsvXhIGw__",
+    title: "Is Bandhan banking too much on microfinance?",
+    description: `  In trying to keep costs in check at his loss-making airline, chairman Ajay Singh may have created a toxic work atmosphere and poor safety culture.`,
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    articleReadTime: 5,
+    commentsCount: 23,
+  },
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/460b/7ba2/cef7f3b55ea636ab34295402fb4c9c30?Expires=1732492800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XeOd0EixisX5Mf-vkl8xYREMNWMSEuiJuW7KWMJWxvX8X3HBnZcE2QKOgIXxNtSgX5hsMczjhXgn59yEncGO-wki5TWCguZxSCKOrcLqvYhoReJzLAbqJZPhOcn~liV8TsnZQI5gt7BcpVEbzIa0BGSTZkkfZObZ7S8FFu-dk2w-sN0tC04hIUdqQW19jzXXS10HjgHPb4sumyRLPIH1qdlSHn~pAFClhFeCA~LNbCUd2XwQ94aRICMjp-Mfj1Ht6RfiRAnUzbAV4Ai1aviS8AV3YxrQ0uCXi7Ymo5~MNaiTkaVR2cFg0WaNMmMmr~Uv~6pQAw-NHUtWZKZga~p5OQ__",
+    title: "Is Bandhan banking too much on microfinance?",
+    description: `  In trying to keep costs in check at his loss-making airline, chairman Ajay Singh may have created a toxic work atmosphere and poor safety culture.`,
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    articleReadTime: 5,
+    commentsCount: 23,
+  },
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/c01d/9f9a/6f38c048c3ba6fdda30589521ca80cd8?Expires=1732492800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EIv-iAXd0meJZKa5Jm229dzTX2GebXwk4hP3rP~yBArSWOP2CMqeIhv-Rtm0tlvAtW4o8R8s-1gKAEvWPv5ufUaDxPmUZe~lqYu31NauSTidymHYj2dwz6PVkGdcwYJ46rSRgFRL7~s6xge7OI96QZZT017p-MD8lv9-cT1WLJxaQu8eyM2-MoO1edYHVywFMuBKGzqEUU8Z7RmK32DP7QnNYw9MvPp7ocMMUABJLJ5rG6jM6MDYeMZcrF~a5f8lYK1r9CQPDXuPDMilZImRZwTjJpdDALh0SMWyhO9My9K2qb7FyHKMK0M56qs4zpljc3EsszDJkHYRlN0YRxeDYg__",
+    title: "Is Bandhan banking too much on microfinance?",
+    description: `  In trying to keep costs in check at his loss-making airline, chairman Ajay Singh may have created a toxic work atmosphere and poor safety culture.`,
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    articleReadTime: 5,
+    commentsCount: 23,
+  },
+];
+
+export const hashTagsData = [
+  "fintech",
+  "healthtech",
+  "edtech",
+  "e-commerce",
+  "a",
+  "cybersecurity",
+  "foodtech",
+  "traveltech",
+  "web3",
+  "cleantech",
+  "agritech",
+  "fashiontech",
+  "biotech",
+  "gaming",
+  "wellness",
+  "energy",
+  "space",
+  "vr",
+];
+
+export const logos = [
+  {
+    src: "facebook.svg",
+  },
+  {
+    src: "twitter.svg",
+  },
+  {
+    src: "linkedin.svg",
+  },
+  {
+    src: "instagram.svg",
+  },
+  {
+    src: "youtube.svg",
+  },
+];
+
+export const files = [
+  "adobe",
+  "afterpay",
+  "figma",
+  "evernote",
+  "paypal",
+  "outreach",
+  "adobe",
+  "afterpay",
+  "figma",
+  "evernote",
+];
