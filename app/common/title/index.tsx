e0cc752cd18c0efa28fdfddd442c4521e4f74881
nirajsathe80 <nirajsathe80@gmail.com>
2024-11-14 11:41:29 +0530

    generic title changed for desktop screens

--- app/common/title/index.tsx
+++ app/common/title/index.tsx
@@ -1,10 +1,19 @@
-const Title = ({ title, className }: { title: string; className?: string }) => {
+const Title = ({
+  title,
+  className = "",
+}: {
+  title: string;
+  className?: string;
+}) => {
   return (
-    <h1
-      className={`font-bold text-[#292929] text-3xl text-center ${className}`}
-    >
-      {title}
-    </h1>
+    <div className="relative">
+      <div className="border-[0.5px] border-[#00000099] hidden sm:block"></div>
+      <h1
+        className={`font-bold text-[#292929] relative text-3xl text-center sm:absolute sm:left-[50%] sm:top-[-19px] sm:-translate-x-1/2 ${className}`}
+      >
+        {title}
+      </h1>
+    </div>
   );
 };
 
